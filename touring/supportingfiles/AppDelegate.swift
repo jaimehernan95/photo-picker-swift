b0f84d849379211afe8954f23a3ff591bcdf8dd8
jaimea <jaime.hernan95@gmail.com>
2020-02-03 14:18:54 -0500

    Bug fixed. GoogleService-Info.plist not found.  Drag and drop doesn't work. need to add files to add GoogleService-Info.plist to the project. Solution all pod files were removed, after that GoogleService-Info.plist installed. Then  reinstall all pod files. Finally Product clean and run.

--- touring/supportingfiles/AppDelegate.swift
+++ touring/supportingfiles/AppDelegate.swift
@@ -12,14 +12,13 @@ import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?  
+    var window: UIWindow?
     
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
